425fc5347b25da9a2ac7a009b70b51f810dd55bd
drnevich <mdkdrnevich@gmail.com>
2018-04-14 19:58:45 +0200

    Added capabilities to nn_classes.CollisionDataset

--- nn_classes.py
+++ nn_classes.py
@@ -1,176 +1,62 @@
 from __future__ import print_function, division
 import pandas as pd
+import numpy as np
+import random
 from sklearn import preprocessing
 import torch as th
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset
 
+try:
+    xrange
+except NameError:
+    xrange = range
 
 class CollisionDataset(Dataset):
-    def __init__(self, datafile, header=None, scaler=None, target_col=0, index_col=None):
-        self.dataframe = pd.read_csv(datafile, header=header, index_col=index_col)
-        self._X = pd.concat([self.dataframe.iloc[:, :target_col], self.dataframe.iloc[:, target_col+1:]], axis=1)
-        self._y = self.dataframe.iloc[:, target_col]
-        self.scaler = preprocessing.StandardScaler().fit(self._X) if scaler is None else scaler
-        self._tX = th.from_numpy(self.scaler.transform(self._X.as_matrix()))
-        self._tY = th.from_numpy(self._y.as_matrix())
+    def __init__(self, datafile, filetype, header=None, scaler=None, target_col=0, index_col=None):
+        if filetype.lower() == "pandas":
+            dataframe = pd.read_csv(datafile, header=header, index_col=index_col)
+            X = pd.concat([dataframe.iloc[:, :target_col], dataframe.iloc[:, target_col+1:]], axis=1).as_matrix()
+            y = dataframe.iloc[:, target_col].as_matrix()
+        elif filetype.lower() == "numpy":
+            M = np.load(datafile)
+            X = np.concatenate([M[:, :target_col], M[:, target_col+1:]], axis=1)
+            y = M[:, target_col]
+            
+        self.scaler = preprocessing.StandardScaler().fit(X) if scaler is None else scaler
+        self._tX = th.from_numpy(self.scaler.transform(X)).float()
+        self._tY = th.from_numpy(y).type(th.ByteTensor)
+            
     
     def __len__(self):
-        return len(self.dataframe)
+        return len(self._tY)
+    
     
     def __getitem__(self, idx):
-        if type(idx) is int:
-            return {'input': self._tX[idx, :].view(1, -1),
-                    'target': self._tY[idx]}
-            #return {'input': th.from_numpy(self.scaler.transform(self._X.iloc[idx, :].as_matrix().reshape(1, -1)).flatten()),
-            #        'target': self._y.iloc[idx]}
-        #return {'input': th.from_numpy(self.scaler.transform(self._X.iloc[idx, :].as_matrix())),
-        #        'target': th.from_numpy(self._y.iloc[idx].as_matrix())}
-        return {'input': self._tX[idx, :], 'target': self._tY[idx]}
+        return {'input': self._tX[idx].contiguous().view(1, -1) if type(idx) is int else self._tX[idx, :],
+                'target': self._tY[idx]}
     
-
-# Basic Forward Prop ReLu 
-class MultiNet(nn.Module):
-    def __init__(self):
-        super(MultiNet, self).__init__()
-        self.lin1 = nn.Linear(3, 7)
-        self.lin2 = nn.Linear(7, 3)
-
-    def forward(self, x):
-        x = F.relu(self.lin1(x))
-        x = F.softmax(self.lin2(x), dim=1)
-        return x
     
-class BinaryNet(nn.Module):
-    def __init__(self, input_dim):
-        super(BinaryNet, self).__init__()
-        self.lin1 = nn.Linear(input_dim, (input_dim+1)//2)
-        self.lin2 = nn.Linear((input_dim+1)//2, 1)
-
-    def forward(self, x):
-        x = F.relu(self.lin1(x))
-        x = F.sigmoid(self.lin2(x))
-        return x
+    def subsample(self, size):
+        datasize = len(self)
+        if 0 < size <= 1:
+            cut = int(size*datasize)
+        elif size > 1:
+            cut = min((int(size), datasize))
+        else:
+            return None
+        subsample = random.sample(xrange(datasize), cut)
+        self._tX = self._tX[subsample]
+        self._tY = self._tY[subsample]
+        
+        
+    def saveas(self, filename, filetype):
+        if filetype.lower() == 'numpy':
+            np.save(filename, np.concatenate((self._tY.view(-1, 1).numpy(), self.scaler.inverse_transform(self._tX)), axis=1))
+            
     
-class DeepBinaryNet(nn.Module):
-    def __init__(self, input_dim):
-        super(DeepBinaryNet, self).__init__()
-        self.lin1 = nn.Linear(input_dim, input_dim*2)
-        self.lin2 = nn.Linear(input_dim*2, input_dim*5)
-        self.lin3 = nn.Linear(input_dim*5, input_dim*10)
-        self.lin4 = nn.Linear(input_dim*10, input_dim*15)
-        self.lin5 = nn.Linear(input_dim*15, input_dim*10)
-        self.lin6 = nn.Linear(input_dim*10, input_dim*5)
-        self.lin7 = nn.Linear(input_dim*5, input_dim*2)
-        self.lin8 = nn.Linear(input_dim*2, input_dim)
-        self.lin9 = nn.Linear(input_dim, input_dim//2)
-        self.lin10 = nn.Linear(input_dim//2, 1)
-
-
-    def forward(self, x):
-        x = F.relu(self.lin1(x))
-        x = F.relu(self.lin2(x))
-        x = F.relu(self.lin3(x))
-        x = F.relu(self.lin4(x))
-        x = F.relu(self.lin5(x))
-        x = F.relu(self.lin6(x))
-        x = F.relu(self.lin7(x))
-        x = F.relu(self.lin8(x))
-        x = F.relu(self.lin9(x))
-        x = F.sigmoid(self.lin10(x))
-        return x
-    
-class DeepBinaryDropNet(nn.Module):
-    def __init__(self, input_dim):
-        super(DeepBinaryDropNet, self).__init__()
-        self.lin1 = nn.Linear(input_dim, input_dim*2)
-        self.lin2 = nn.Linear(input_dim*2, input_dim*5)
-        self.lin3 = nn.Linear(input_dim*5, input_dim*10)
-        self.lin4 = nn.Linear(input_dim*10, input_dim*15)
-        self.lin5 = nn.Linear(input_dim*15, input_dim*10)
-        self.lin6 = nn.Linear(input_dim*10, input_dim*5)
-        self.lin7 = nn.Linear(input_dim*5, input_dim*2)
-        self.lin8 = nn.Linear(input_dim*2, input_dim)
-        self.lin9 = nn.Linear(input_dim, input_dim//2)
-        self.lin10 = nn.Linear(input_dim//2, 1)
-        self.dropout = nn.Dropout()
-
-
-    def forward(self, x):
-        x = F.relu(self.lin1(x))
-        x = F.relu(self.lin2(x))
-        x = F.relu(self.dropout(self.lin3(x)))
-        x = F.relu(self.lin4(x))
-        x = F.relu(self.lin5(x))
-        x = F.relu(self.dropout(self.lin6(x)))
-        x = F.relu(self.lin7(x))
-        x = F.relu(self.lin8(x))
-        x = F.relu(self.dropout(self.lin9(x)))
-        x = F.sigmoid(self.lin10(x))
-        return x
-    
-class DeepFunnelNet(nn.Module):
-    def __init__(self, input_dim):
-        super(DeepFunnelNet, self).__init__()
-        self.lin1 = nn.Linear(input_dim, 25)
-        self.lin2 = nn.Linear(25, 25)
-        self.lin3 = nn.Linear(25, 20)
-        self.lin4 = nn.Linear(20, 20)
-        self.lin5 = nn.Linear(20, 15)
-        self.lin6 = nn.Linear(15, 15)
-        self.lin7 = nn.Linear(15, 10)
-        self.lin8 = nn.Linear(10, 10)
-        self.lin9 = nn.Linear(10, 5)
-        self.lin10 = nn.Linear(5, 5)
-        self.lin11 = nn.Linear(5, 1)
-        self.dropout = nn.Dropout()
-
-
-    def forward(self, x):
-        x = F.relu(self.lin1(x))
-        x = F.relu(self.lin2(x))
-        x = F.relu(self.dropout(self.lin3(x)))
-        x = F.relu(self.lin4(x))
-        x = F.relu(self.lin5(x))
-        x = F.relu(self.dropout(self.lin6(x)))
-        x = F.relu(self.lin7(x))
-        x = F.relu(self.lin8(x))
-        x = F.relu(self.dropout(self.lin9(x)))
-        x = F.relu(self.lin10(x))
-        x = F.sigmoid(self.lin11(x))
-        return x
-    
-class DeepConstantNet(nn.Module):
-    def __init__(self, input_dim):
-        super(DeepConstantNet, self).__init__()
-        self.lin1 = nn.Linear(input_dim, input_dim*3//2)
-        self.lin2 = nn.Linear(input_dim*3//2, input_dim*3//2)
-        self.lin3 = nn.Linear(input_dim*3//2, input_dim*3//2)
-        self.lin4 = nn.Linear(input_dim*3//2, input_dim*3//2)
-        self.lin5 = nn.Linear(input_dim*3//2, input_dim*3//2)
-        self.lin6 = nn.Linear(input_dim*3//2, input_dim*3//2)
-        self.lin7 = nn.Linear(input_dim*3//2, input_dim*3//2)
-        self.lin8 = nn.Linear(input_dim*3//2, input_dim)
-        self.lin9 = nn.Linear(input_dim, input_dim//2)
-        self.lin10 = nn.Linear(input_dim//2, 1)
-        self.dropout = nn.Dropout()
-
-
-    def forward(self, x):
-        x = F.relu(self.lin1(x))
-        x = F.relu(self.lin2(x))
-        x = F.relu(self.dropout(self.lin3(x)))
-        x = F.relu(self.lin4(x))
-        x = F.relu(self.lin5(x))
-        x = F.relu(self.dropout(self.lin6(x)))
-        x = F.relu(self.lin7(x))
-        x = F.relu(self.lin8(x))
-        x = F.relu(self.dropout(self.lin9(x)))
-        x = F.sigmoid(self.lin10(x))
-        return x
-    
-class DeepBinaryRegNet(nn.Module):
+class DHTTNet(nn.Module):
     def __init__(self, input_dim):
         super(DeepBinaryRegNet, self).__init__()
         # Layers
