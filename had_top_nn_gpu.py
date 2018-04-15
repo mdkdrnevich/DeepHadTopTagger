c710eacf15af85987c15f98dfa4bce63cb8f7773
drnevich <mdkdrnevich@gmail.com>
2018-04-16 00:08:34 +0200

    Updated the training script for the GPU

--- had_top_nn_gpu.py
+++ had_top_nn_gpu.py
@@ -24,198 +24,65 @@ import utils
 # ## Load the Datasets
 # Here I load the datasets using my custom <code>Dataset</code> class. This ensures that the data is scaled properly and then the PyTorch <code>DataLoader</code> shuffles and iterates over the dataset in batches.
 
-trainset = utils.CollisionDataset("../../scratch/ttH_hadT_cut_train.csv", header=0, target_col=0, index_col=0)
-valset = utils.CollisionDataset("../../scratch/ttH_hadT_cut_val.csv", header=0, target_col=0, index_col=0, scaler=trainset.scaler)
-testset = utils.CollisionDataset("../../scratch/ttH_hadT_cut_test.csv", header=0, target_col=0, index_col=0, scaler=trainset.scaler)
+trainset = utils.CollisionDataset("training_basic_set.npy")
+valset = utils.CollisionDataset("validation_basic_set.npy", scaler=trainset.scaler)
+testset = utils.CollisionDataset("testing_basic_set.npy", scaler=trainset.scaler)
 
 trainloader = DataLoader(trainset, batch_size=512, shuffle=True, num_workers=5)
 testloader = DataLoader(testset, batch_size=512, shuffle=True, num_workers=5)
 
-train_X = Variable(trainset[:]['input']).float()
-train_y = trainset[:]['target'].long().view(-1, 1).numpy()
+train_X = Variable(trainset[:]['input'])
+train_y = trainset[:]['target'].numpy()
 
-val_X = Variable(valset[:]['input']).float()
-val_y = valset[:]['target'].long().view(-1, 1).numpy()
+val_X = Variable(valset[:]['input'])
+val_y = valset[:]['target'].numpy()
 
 # ## Initialize the NN, Loss Function, and Optimizer
 
+input_dim = trainset.shape[1]
 
-#criterion = nn.CrossEntropyLoss()
-"""input_dim = trainset._X.shape[1]
+# # Deep Neural Network on the Basic Features
 
-net = BinaryNet(input_dim).cuda()
+dnet = DHTTNet(input_dim).cuda()
+
 criterion = nn.BCELoss()
-optimizer = optim.Adam(net.parameters())
+optimizer = optim.Adam(dnet.parameters())
 
+#if th.cuda.device_count() > 1:
+#  dnet = nn.DataParallel(dnet)
 
-# ## Train the Neural Network
-
-# In[6]:
-
-train_discriminant = net(train_X).data.cpu().numpy()
-val_discriminant = net(val_X).data.cpu().numpy()
+dnet.eval()
+dnet.cpu()
+train_discriminant = dnet(train_X).data.numpy()
+val_discriminant = dnet(val_X).data.numpy()
+dnet.cuda()
+dnet.train()
 val_curve = [(roc_auc_score(train_y, train_discriminant), roc_auc_score(val_y, val_discriminant))]
 
-print("Training Standard NN")
-for epoch in range(1, 11):
+print("Training DNN")
+for epoch in range(1, 9):
     if epoch%2 == 0: print(epoch)
     for batch in trainloader:
         inputs, targets = Variable(batch['input'].cuda()).float(), Variable(batch['target'].cuda()).float().view(-1, 1)
         optimizer.zero_grad()
         
-        outputs = net(inputs)
+        outputs = dnet(inputs)
         loss = criterion(outputs, targets)
         loss.backward()
         optimizer.step()
 
-    #Evaluate the model on the training set
-    train_discriminant = net(train_X).data.cpu().numpy()
-
-    # Evaluate the model on a validation set
-    val_discriminant = net(val_X).data.cpu().numpy()
-    
-    # Add the ROC AUC to the curve
-    val_curve.append((roc_auc_score(train_y, train_discriminant), roc_auc_score(val_y, val_discriminant)))
-print("Done")
-
-fig, ax = plt.subplots()
-plt.plot(range(1, len(val_curve)+1), val_curve)
-ax.set_ylabel("ROC AUC")
-ax.set_xlabel("Epochs Finished")
-ax.set_title("Validation Curves")
-handles, _ = ax.get_legend_handles_labels()
-labels = ["Training", "Validation"]
-plt.legend(handles, labels, loc='lower right')
-fig.set_size_inches(18, 10)
-fig.savefig("NN_val_curves.png")
-
-# In[8]:
-
-
-th.save(net.state_dict(), "neural_net.torch")"""
-
-
-# # Try Another NN Trained on Just the "Raw" Features
-
-# In[48]:
-
-
-raw_trainset = CollisionDataset("../../scratch/ttH_hadT_cut_raw_train.csv", header=0, target_col=0, index_col=0)
-raw_valset = CollisionDataset("../../scratch/ttH_hadT_cut_raw_val.csv", header=0, target_col=0, index_col=0, scaler=raw_trainset.scaler)
-raw_testset = CollisionDataset("../../scratch/ttH_hadT_cut_raw_test.csv", header=0, target_col=0, index_col=0, scaler=raw_trainset.scaler)
-
-raw_trainloader = DataLoader(raw_trainset, batch_size=512, shuffle=True, num_workers=5)
-raw_testloader = DataLoader(raw_testset, batch_size=512, shuffle=True, num_workers=5)
-
-raw_train_X = Variable(raw_trainset[:]['input']).float()
-raw_train_y = raw_trainset[:]['target'].long().view(-1, 1).numpy()
-
-raw_val_X = Variable(raw_valset[:]['input']).float()
-raw_val_y = raw_valset[:]['target'].long().view(-1, 1).numpy()
-
-# In[10]:
-
-
-raw_input_dim = raw_trainset._X.shape[1]
-
-"""raw_net = BinaryNet(raw_input_dim).cuda()
-raw_criterion = nn.BCELoss()
-raw_optimizer = optim.Adam(raw_net.parameters())
-
-
-# In[11]:
-
-train_discriminant = raw_net(raw_train_X).data.cpu().numpy()
-val_discriminant = raw_net(raw_val_X).data.cpu().numpy()
-val_curve = [(roc_auc_score(raw_train_y, train_discriminant), roc_auc_score(raw_val_y, val_discriminant))]
-
-print("Training Basic NN")
-for epoch in range(1, 11):
-    if epoch%2 == 0: print(epoch)
-    for batch in raw_trainloader:
-        inputs, targets = Variable(batch['input'].cuda()).float(), Variable(batch['target'].cuda()).float().view(-1, 1)
-        raw_optimizer.zero_grad()
-        
-        outputs = raw_net(inputs)
-        loss = raw_criterion(outputs, targets)
-        loss.backward()
-        raw_optimizer.step()
-
-    #Evaluate the model on the training set
-    train_discriminant = raw_net(raw_train_X).data.cpu().numpy()
-
-    # Evaluate the model on a validation set
-    val_discriminant = raw_net(raw_val_X).data.cpu().numpy()
-    
-    # Add the ROC AUC to the curve
-    val_curve.append((roc_auc_score(raw_train_y, train_discriminant), roc_auc_score(raw_val_y, val_discriminant)))
-print("Done")
-
-fig, ax = plt.subplots()
-plt.plot(range(1, len(val_curve)+1), val_curve)
-ax.set_ylabel("ROC AUC")
-ax.set_xlabel("Epochs Finished")
-ax.set_title("Validation Curves")
-handles, _ = ax.get_legend_handles_labels()
-labels = ["Training", "Validation"]
-plt.legend(handles, labels, loc='lower right')
-fig.set_size_inches(18, 10)
-fig.savefig("basicNN_val_curves.png")
-
-
-# In[12]:
-
-
-th.save(raw_net.state_dict(), "raw_neural_net.torch")"""
-
-
-# # Deep Neural Network on the Basic Features
-
-# In[13]:
-
-
-dnet = DeepBinaryRegNet(raw_input_dim).cuda()
-
-deep_criterion = nn.BCELoss()
-deep_optimizer = optim.Adam(dnet.parameters())
-
-#if th.cuda.device_count() > 1:
-#  dnet = nn.DataParallel(dnet)
-
-# In[14]:
-
-dnet.eval()
-dnet.cpu()
-train_discriminant = dnet(raw_train_X).data.numpy()
-val_discriminant = dnet(raw_val_X).data.numpy()
-dnet.cuda()
-dnet.train()
-val_curve = [(roc_auc_score(raw_train_y, train_discriminant), roc_auc_score(raw_val_y, val_discriminant))]
-
-print("Training DNN")
-for epoch in range(1, 9):
-    if epoch%2 == 0: print(epoch)
-    for batch in raw_trainloader:
-        inputs, targets = Variable(batch['input'].cuda()).float(), Variable(batch['target'].cuda()).float().view(-1, 1)
-        deep_optimizer.zero_grad()
-        
-        outputs = dnet(inputs)
-        loss = deep_criterion(outputs, targets)
-        loss.backward()
-        deep_optimizer.step()
-
     dnet.cpu()
     dnet.eval()
     #Evaluate the model on the training set
-    train_discriminant = dnet(raw_train_X).data.numpy()
+    train_discriminant = dnet(train_X).data.numpy()
 
     # Evaluate the model on a validation set
-    val_discriminant = dnet(raw_val_X).data.numpy()
+    val_discriminant = dnet(val_X).data.numpy()
     dnet.cuda()
     dnet.train()
     
     # Add the ROC AUC to the curve
-    val_curve.append((roc_auc_score(raw_train_y, train_discriminant), roc_auc_score(raw_val_y, val_discriminant)))
+    val_curve.append((roc_auc_score(train_y, train_discriminant), roc_auc_score(val_y, val_discriminant)))
 print("Done")
 
 fig, ax = plt.subplots()
@@ -229,15 +96,6 @@ plt.legend(handles, labels, loc='lower right')
 fig.set_size_inches(18, 10)
 fig.savefig("DNN_val_curves.png")
 
-# In[16]:
-
 dnet.eval()
 dnet.cpu()
-th.save(dnet.state_dict(), "deep_basic_neural_net.torch")
-
-
-# In[97]:
-
-
-#cut = find_cut(net, trainset, benchmark="acc")
-#score(net, valset, cut=cut)
+th.save(dnet.state_dict(), "neural_net.torch")
