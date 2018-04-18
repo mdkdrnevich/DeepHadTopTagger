3d50ddf7c2886333762c5f8c1c21515cb41c8cb0
drnevich <mdkdrnevich@gmail.com>
2018-04-18 12:29:31 +0200

    Improved preprocessing and added the ability to save test sets

--- preprocessing.py
+++ preprocessing.py
@@ -11,12 +11,14 @@ import utils
 import argparse
 import glob
 import os
+import os.path as ospath
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-b", "--background")
 parser.add_argument("-s", "--signal")
 parser.add_argument("-n", "--name", help="Name of the sample that you want added to the saved datafile names", default="")
 parser.add_argument("-x", "--exclude", action="store_true", help="Exclude the engineered variables")
+parser.add_argument("-t", "--test", action="store_true", help="Save testing sets separately as <file>_test.npy")
 args = parser.parse_args()
 
 if args.background:
@@ -74,38 +76,59 @@ for dfile in bkgd_files:
     
 print("Finished loading files. The smallest file after Top Mass cuts was {}".format(smallest))
 
-for ix, dset in enumerate(cut_signals):
-    dset.subsample(smallest)
-    if ix == 0:
-        total_signal = dset
-    else:
-        total_signal = total_signal + dset
-        
-for ix, dset in enumerate(cut_bkgds):
-    dset.subsample(smallest)
-    if ix == 0:
-        total_bkgd = dset
-    else:
-        total_bkgd = total_bkgd + dset
-
 # #### Make the different sets
 # Save the Datasets as Training, Validation, and Testing Sets to Facilitate Easy Use in PyTorch and Scikit Learn
+# I ensure that there is an equal amount of each class in each set
 # Dataset Fractions:
 # - Training: 65%
 # - Validation: 15%
 # - Testing: 20%
 
-print("Saving datasets")
-
-total_signal.shuffle()
-total_bkgd.shuffle()
-
 ix_train_cut = int(0.6*smallest)
 ix_val_cut = ix_train_cut + int(0.15*smallest)
 
-train = total_signal.slice(0, ix_train_cut) + total_bkgd.slice(0, ix_train_cut)
-val = total_signal.slice(ix_train_cut, ix_val_cut) + total_bkgd.slice(ix_train_cut, ix_val_cut)
-test = total_signal.slice(ix_val_cut, smallest) + total_bkgd.slice(ix_val_cut, smallest)
+sig_scale = len(bkgd_files)/len(signal_files) if len(signal_files) > len(bkgd_files) else 1
+bkgd_scale = len(signal_files)/len(bkgd_files) if len(bkgd_files) > len(signal_files) else 1
+
+for ix, dset in enumerate(cut_signals):
+    dset.subsample(smallest)
+    dset.shuffle()
+    if args.test:
+            name = ospath.splitext(signal_files[ix])[0] + "_test.npy"
+            dset.slice(ix_val_cut, smallest).saveas(name)
+    if ix == 0:
+        total_train_signal = dset.slice(0, int(ix_train_cut * sig_scale))
+        total_val_signal = dset.slice(int(ix_train_cut * sig_scale), int(ix_val_cut * sig_scale))
+        total_test_signal = dset.slice(int(ix_val_cut * sig_scale), int(smallest * sig_scale))
+    else:
+        total_train_signal = total_train_signal + dset.slice(0, int(ix_train_cut * sig_scale))
+        total_val_signal = total_val_signal + dset.slice(int(ix_train_cut * sig_scale), int(ix_val_cut * sig_scale))
+        total_test_signal = total_test_signal + dset.slice(int(ix_val_cut * sig_scale), int(smallest * sig_scale))
+        
+for ix, dset in enumerate(cut_bkgds):
+    dset.subsample(smallest)
+    dset.shuffle()
+    if args.test:
+            name = ospath.splitext(bkgd_files[ix])[0] + "_test.npy"
+            dset.slice(ix_val_cut, smallest).saveas(name)
+    if ix == 0:
+        total_train_bkgd = dset.slice(0, int(ix_train_cut * bkgd_scale))
+        total_val_bkgd = dset.slice(int(ix_train_cut * bkgd_scale), int(ix_val_cut * bkgd_scale))
+        total_test_bkgd = dset.slice(int(ix_val_cut * bkgd_scale), int(smallest * bkgd_scale))
+    else:
+        total_train_bkgd = total_train_signal + dset.slice(0, int(ix_train_cut * bkgd_scale))
+        total_val_bkgd = total_val_signal + dset.slice(int(ix_train_cut * bkgd_scale), int(ix_val_cut * bkgd_scale))
+        total_test_bkgd = total_test_signal + dset.slice(int(ix_val_cut * bkgd_scale), int(smallest * bkgd_scale))
+
+print("Saving datasets")
+
+train = total_train_signal + total_train_bkgd
+val = total_val_signal + total_val_bkgd
+test = total_test_signal + total_test_bkgd
+
+train.shuffle()
+val.shuffle()
+test.shuffle()
 
 if not args.exclude:
     train.saveas(args.name + "training_set.npy")
