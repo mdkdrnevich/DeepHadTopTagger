d873ba2c6dcac24b39c35d6106bbff4a57e412c9
drnevich <mdkdrnevich@gmail.com>
2018-05-02 17:31:39 +0200

    Added a macro to generate events with the indices of the hadronic triplet

---
+++ makeTagTripletsSet.C
@@ -0,0 +1,194 @@
+//Charlie Mueller 2/24/2016
+#include <iostream>
+#include <fstream>
+#include "TSystem.h"
+#include <vector>
+#include "TH1.h"
+#include "TChain.h"
+#include <string>
+#include <algorithm>
+#include "TString.h"
+#include "TH1D.h"
+#include "TFile.h"
+#include <cmath>
+#include "TLorentzVector.h"
+#include "ttH-13TeVMultiLeptons/TemplateMakers/src/classes.h"
+#include "TMVA/Config.h"
+#include "TMVA/Tools.h"
+#include "TMVA/Reader.h"
+#include "TMVA/MethodCuts.h"
+#include "selection.h"
+#include "loadSamples.h"
+#include "treeTools.h"
+#include "TCanvas.h"
+#include "TH2I.h"
+#include "DataFormats/Math/interface/deltaR.h"
+
+/////////////////////////////////////////
+///
+/// usage: root -l makeSelectionTrees.C+
+///
+/////////////////////////////////////////
+
+struct less_than_key
+{
+    inline bool operator() (const ttH::Jet& struct1, const ttH::Jet& struct2)
+    {
+        return ((struct1.DeepCSVprobb + struct1.DeepCSVprobbb) < (struct2.DeepCSVprobb + struct2.DeepCSVprobbb));
+    }
+};
+
+void write_csv(std::ofstream& in_file, vector<ttH::Jet> in_jets, vector<int> indices)
+{  
+  for (int i=0; i<3; i++) {
+    in_file << indices[i];
+    if (i<2)
+        in_file << ".";
+    else
+        in_file << ",";
+  }
+
+  //sort(in_jets.begin(), in_jets.end(), less_than_key());
+
+  for (const auto & jet : in_jets)
+    {
+      in_file<< jet.obj.pt() << "," << jet.obj.eta() <<","<< jet.obj.phi() <<","<< jet.obj.M() <<",";
+      in_file<< jet.charge <<","<< jet.DeepCSVprobb <<","<< jet.DeepCSVprobbb <<",";
+      in_file<< jet.DeepCSVprobc <<","<< jet.DeepCSVprobudsg <<",";
+      in_file<< jet.qgid <<",";
+    }
+  
+  in_file<<"\n";
+  in_file.flush();
+}
+
+void run_it(TChain* tree, TString output_file)
+{
+
+  //int num_hadronic = 0;
+  int treeentries = tree->GetEntries();   
+  cout << "# events in tree: "<< treeentries << endl;  
+
+  double mcwgt_intree = -999.;
+  double wallTimePerEvent_intree = -99.;  
+  int eventnum_intree = -999;
+  int lumiBlock_intree = -999;
+  int runNumber_intree = -999;
+  
+  vector<ttH::Jet> *preselected_jets_intree=0;
+  vector<ttH::GenParticle> *gen_parts=0;
+  
+  tree->SetBranchStatus("*",0);
+  tree->SetBranchStatus("mcwgt",1);
+  tree->SetBranchStatus("wallTimePerEvent",1);
+  tree->SetBranchStatus("eventnum",1);
+  tree->SetBranchStatus("lumiBlock",1);
+  tree->SetBranchStatus("preselected_jets.*",1);
+  tree->SetBranchStatus("pruned_genParticles.*",1);
+
+
+  tree->SetBranchAddress("mcwgt", &mcwgt_intree);
+  tree->SetBranchAddress("wallTimePerEvent", &wallTimePerEvent_intree);
+  tree->SetBranchAddress("eventnum", &eventnum_intree);
+  tree->SetBranchAddress("lumiBlock", &lumiBlock_intree);
+  tree->SetBranchAddress("preselected_jets", &preselected_jets_intree);
+  tree->SetBranchAddress("pruned_genParticles", &gen_parts);
+  
+  ofstream output_file;
+  outfile.open(output_file);
+
+  Int_t cachesize = 500000000;   //500 MBytes
+  tree->SetCacheSize(cachesize);
+  tree->SetCacheLearnEntries(20); 
+
+
+  double starttime = get_wall_time();
+  //  treeentries = 1000000;
+  int num_signal = 0;
+  int num_bkgd = 0;
+  float avg_bkgd = 0;
+  for (int i=0; i<treeentries; i++)
+    {
+
+      //printProgress(i,treeentries);
+      tree->GetEntry(i);
+      
+      cout<< "Finished: " <<(i+1)*100/treeentries <<"%\r";
+
+      if (preselected_jets_intree->size() < 3) continue;
+      vector<ttH::Jet> matched_jets[2] = {vector<ttH::Jet>(), vector<ttH::Jet>()};
+      vector<int> matched_ix[2] = {vector<int>(), vector<int>()};
+      vector<ttH::Jet> unmatched_jets;
+      int ix = 0;
+      for (const auto &pjet : *preselected_jets_intree) {
+        if (pjet.genPdgID == 5 && pjet.genMotherPdgID == 6) {
+          matched_jets[0].insert(matched_jets[0].begin(), pjet);
+          matched_ix[0].push_back(ix);
+        } else if (pjet.genPdgID == -5 && pjet.genMotherPdgID == -6) {
+          matched_jets[1].insert(matched_jets[1].begin(), pjet);
+          matched_ix[1].push_back(ix);
+        } else if (pjet.genMotherPdgID == 24 && pjet.genGrandMotherPdgID == 6) {
+          matched_jets[0].push_back(pjet);
+          matched_ix[0].push_back(ix);
+        } else if (pjet.genMotherPdgID == -24 && pjet.genGrandMotherPdgID == -6) {
+          matched_jets[1].push_back(pjet);
+          matched_ix[1].push_back(ix);
+        } else {
+          unmatched_jets.push_back(pjet);
+        }
+        ix++;
+      }
+      
+      // Make sure there's a hadronic top
+      if ((matched_jets[0].size() != 3) && (matched_jets[1].size() != 3))
+        continue;
+          
+      // Generate all combinations of indices via 3 for loops
+      // Every iteration make a vector to compare against matched vectors if there is a fully matched triplet
+      // Write the triplet to file
+      int size = preselected_jets_intree->size();
+      bool matched1 = (matched_jets[0].size() == 3);
+      bool matched2 = (matched_jets[1].size() == 3);
+      for (int i=0; i < size-2; i++) {
+        for (int j=i+1; j < size-1; j++) {
+          for (int k=j+1; k < size; k++) {
+            vector<int> comb = {i, j, k};
+            if (matched1 && (comb == matched_ix[0])) {
+                write_csv(outfile, *preselected_jets_intree, comb);
+            } else if (matched2 && (comb == matched_ix[1])) {
+                write_csv(outfile, *preselected_jets_intree, comb);
+            }
+          }
+        }
+      }
+    }
+
+  double endtime = get_wall_time();
+  cout << "Elapsed time: " << endtime - starttime << " seconds, " << endl;
+  if (treeentries>0) cout << "an average of " << (endtime - starttime) / treeentries << " per event." << endl;
+  cout << "Num Sig: " <<num_signal<< endl;
+  cout << "Num Bkgd: " <<num_bkgd<< endl;
+  cout << "Avg Bkgd: " <<avg_bkgd<< endl;
+  
+  outfile.close();
+}
+
+void makeTagTripletsSet(TString sample="")
+{
+
+  TString output_dir = "";
+  TString input_files[7] = {"trees/output_tree_15290.root", "trees/output_tree_18889.root",
+                            "trees/output_tree_25083.root", "trees/output_tree_21531.root",
+                            "trees/output_tree_18046.root", "trees/output_tree_69439.root",
+                            "trees/output_tree_39898.root"};
+  
+  sample = "ttH";
+  TString output_file = output_dir + sample + "_triplets.csv";
+  TChain *tth_chain = new TChain("OSTwoLepAna/summaryTree");    
+  for (int i=0; i<7; i++) {
+    tth_chain->Add(input_files[i]);
+  }
+
+  run_it(tth_chain, output_file);
+
+}
