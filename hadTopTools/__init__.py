e928467ae8f08f67968fe37e0876b28b46bb622f
drnevich <mdkdrnevich@gmail.com>
2018-08-01 13:32:52 -0500

    Fixed typo

--- hadTopTools/__init__.py
+++ hadTopTools/__init__.py
@@ -2,4 +2,4 @@ import matplotlib as mpl
 mpl.use("Agg")
 
 from .utils import CollisionDataset, AutoencoderDataset, train, test, plot_curves
-import .nn_classes as nn
+from . import nn_classes as nn
